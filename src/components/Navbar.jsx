9b58324648bbd2e02567b4b13c3aa7d97e340fdd
rcerney <ryancerney1@gmail.com>
2025-06-21 10:09:55 -0700

    made svg images to replace placeholders

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,22 +16,27 @@ export default function Navbar() {
             <div className="cs-top-bar">
                 <div className="cs-top-container">
                     <div className="cs-top-contact">
+                        {/* add link url */}
                         <a href="" className="cs-top-link">
-                            <img className="cs-link-icon" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/Icons/phone-grey.svg" alt="logo" width="16" height="16" aria-hidden="true" decoding="async" />
+                            <img className="cs-link-icon" src="src/assets/phone.svg" alt="logo" width="16" height="16" aria-hidden="true" decoding="async" />
                             Call us: +84 1985 33 999
                         </a>
+                        {/* add link url */}
                         <a href="" className="cs-top-link">
-                            <img className="cs-link-icon" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/Icons/clock-grey.svg" alt="logo" width="20" height="20" aria-hidden="true" decoding="async" />
+                            <img className="cs-link-icon" src="src/assets/clock.svg" alt="logo" width="20" height="20" aria-hidden="true" decoding="async" />
                             Opening hours: Mon - Sat: 8:00am To 9:00pm
                         </a>
                     </div>
                     <div className="cs-top-social">
+                        {/* add link url */}
                         <a href="" className="cs-social-link">
                             <img className="cs-social-icon" src="src/assets/facebook.svg" alt="logo" width="12" height="12" aria-hidden="true" decoding="async" />
                         </a>
+                        {/* add link url */}
                         <a href="" className="cs-social-link">
                             <img className="cs-social-icon" src="src/assets/twitter.svg" alt="logo" width="12" height="12" aria-hidden="true" decoding="async" />
                         </a>
+                        {/* add link url */}
                         <a href="" className="cs-social-link">
                             <img className="cs-social-icon" src='src/assets/linkedin.svg' alt="logo" width="2000" height="200" aria-hidden="true" decoding="async" />
                         </a>
@@ -40,6 +45,7 @@ export default function Navbar() {
                 </div>
             </div>
             <div className="cs-container">
+                {/* add link url */}
                 <a href="" className="cs-logo" aria-label="back to home">
                     <img src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/Logos/consulting-logo.svg" alt="logo" width="197" height="43" aria-hidden="true" decoding="async" />
                 </a>
@@ -54,34 +60,41 @@ export default function Navbar() {
                     <div className="cs-ul-wrapper">
                         <ul id="cs-expanded" className="cs-ul" aria-expanded="false">
                             <li className="cs-li">
+                                {/* add link url */}
                                 <a href="" className="cs-li-link cs-active">Home</a>
                             </li>
                             <li className="cs-li">
+                                {/* add link url */}
                                 <a href="" className="cs-li-link">About</a>
                             </li>
                             <li className="cs-li cs-dropdown" tabIndex="0">
                                 <span className="cs-li-link">
-                                    Services
-                                    <img className="cs-drop-icon" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/Icons/chev-down.svg" alt="dropdown icon" width="15" height="15" decoding="async" aria-hidden="true" />
+                                    Projects
+                                    <img className="cs-drop-icon" src="src/assets/chevron-down.svg" alt="dropdown icon" width="15" height="15" decoding="async" aria-hidden="true" />
                                 </span>
                                 <ul className="cs-drop-ul">
                                     <li className="cs-drop-li">
-                                        <a href="" className="cs-li-link cs-drop-link">Housing</a>
+                                        {/* add link url */}
+                                        <a href="" className="cs-li-link cs-drop-link">Project 1</a>
                                     </li>
                                     <li className="cs-drop-li">
-                                        <a href="" className="cs-li-link cs-drop-link">Food Banks</a>
+                                        {/* add link url */}
+                                        <a href="" className="cs-li-link cs-drop-link">Project 2</a>
                                     </li>
                                 </ul>
                             </li>
                             <li className="cs-li">
-                                <a href="" className="cs-li-link">Blog</a>
+                                {/* add link url */}
+                                <a href="" className="cs-li-link">Reviews</a>
                             </li>
                             <li className="cs-li">
-                                <a href="" className="cs-li-link">Contact</a>
+                                {/* add link url */}
+                                <a href="" className="cs-li-link">Services</a>
                             </li>
                         </ul>
                     </div>
                 </nav>
+                {/* add link url */}
                 <a href="" className="cs-button-solid cs-nav-button">Contact Us</a>
             </div>
         </header>
